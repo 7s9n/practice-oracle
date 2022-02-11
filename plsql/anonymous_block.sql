30b59bdc4fe1901fb5a47879b79abb8f5d0e419a
Ho011 <zzzsssx0@gmail.com>
2022-02-11 04:10:57 +0300

    Add anonymous block example.

---
+++ plsql/anonymous_block.sql
@@ -0,0 +1,52 @@
+-- -----------------------------------------------------------------------------------
+-- File Name    : https://github.com/Ho011/practice-oracle/tree/master/plsql/anonymous_block.sql
+-- Author       : Hussein Sarea
+-- Last Modified: 31/01/2022
+-- -----------------------------------------------------------------------------------
+
+/*
+    A block without a name is an anonymous block.
+    An anonymous block is not saved in the Oracle Database server, so it is just for one-time use.
+    However, PL/SQL anonymous blocks can be useful for testing purposes.
+
+    A PL/SQL block consists of three sections: declaration, executable, and exception-handling sections.
+    In a block, the executable section is mandatory while the declaration and exception-handling sections are optional.
+
+    -- Declaration section
+        A declaration section where you declare variables, 
+        allocate memory for cursors, and define data types.
+    
+    -- Executable section
+        An executable section starts with the keyword BEGIN and ends with the keyword END.
+        The executable section must have a least one executable statement, 
+        even if it is the NULL statement which does nothing.
+    
+    -- Exception-handling section
+        An Exception-handling section starts with the keyword EXCEPTION.
+        The exception-handling section is where you catch and handle exceptions raised by the code in the execution section.
+
+*/
+
+SET SERVEROUTPUT ON;
+
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Hello, World!');
+END;
+/
+
+DECLARE
+    l_message VARCHAR2(50) := 'Hello, world!';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(l_message);
+END;
+/
+
+DECLARE
+    l_result NUMBER;
+BEGIN
+    l_result := 5 / 0;
+
+    EXCEPTION
+        WHEN ZERO_DIVIDE THEN
+            DBMS_OUTPUT.PUT_LINE(SQLERRM);
+END;
